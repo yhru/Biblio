3d78d19d9cfc55c76fe9cf36713e24531d7957a2
JellalEternal <48085028+JellalEternal@users.noreply.github.com>
2019-03-15 13:15:16 +0100

    Add files via upload

--- SQL Biblio + CSS/lib/web/php/functions.php
+++ SQL Biblio + CSS/lib/web/php/functions.php
@@ -1,10 +1,17 @@
 <?php
-function bookdisplay_function (&$donnees){
-	echo '<img src="../../../lib/assets/images/coverpage/'.($donnees['CodeLivre']).'.jpg"/>'.'<br/>'; 
+function bookdisplay_function_result (&$donnees){
+	echo '<img src="../../../lib/assets/images/coverpage/'.($donnees['CodeLivre']).'.jpg"/>'.'<br/>';
+	echo $donnees['Title'] . "<br/>";
+}
+
+function bookdisplay_function_detail (&$donnees){
+	echo '<img src="../../../lib/assets/images/coverpage/'.($donnees['CodeLivre']).'.jpg"/>'.'<br/>';
 	echo $donnees['Title'] . "<br/><br />";
 	echo $donnees['Author'] . "<br/><br />";
 	echo $donnees['Editor'] . "<br/><br />";
+	echo $donnees['PublicationYear'] . "<br/><b/><br/>";
 	echo $donnees['Language'] . "<br/><br />";
-    echo $donnees['Resum'] . "<br/><br /><br /><br />";
+  echo $donnees['Resum'] . "<br/><b/><br/>";
+	echo $donnees['CodeKey'] . "<br/><b/><br/>";
 }
 ?>
