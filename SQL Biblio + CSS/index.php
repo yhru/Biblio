69d82b337459a62aaf1b8bac6a7d9f4b7f078ff2
yhru <48556853+yhru@users.noreply.github.com>
2019-03-21 16:35:05 +0100

    Add files via upload

--- SQL Biblio + CSS/index.php
+++ SQL Biblio + CSS/index.php
@@ -7,7 +7,7 @@
         <title> index.html </title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <label style="font-size:14px; font-family:calibri;"> 
+        <label style="font-size:14px; font-family:calibri;">
         <link rel="stylesheet" type="text/css" href="lib/styles/css/style.css">
     </head>
 
@@ -25,28 +25,66 @@
                 <div class=livreali>
                     <label>Recherche : </label><input class="champ" type="text" name="recherche" placeholder="Saisissez votre recherche..."/>
                     <input class="clean" type="reset" value="&#10006"/>
-                    <input class="search" type="submit" name="1" value="&#10004"><br /><br /><br /><br />
+                    <input class="search" type="submit" name="1" value="&#10004;"><br /><br /><br /><br />
                 </div>
             </form>
-                <div class="cssformulaire">       
+                <div class="cssformulaire">
                     <form name="formulaire2" class="form-search" method="POST" action="lib/web/php/result.php">
                         <strong>Recherche avancée : </strong><br />
                         <span class="form-advanced"><p>Formulaire à plusieurs champs (Veuillez remplir un ou plusieurs champs) :
-                        <input class="clean" type="reset" value="&#10006"/><input  class="search" type="submit" name="2" value="&#10004"></p> 
-                        <label for="Title">Le titre du livre : </label><input class="champ" type="text" name="Title" placeholder="Ex : Harry Potter ..."/><br />        
+                        <input class="clean" type="reset" value="&#10006;"/><input  class="search" type="submit" name="2" value="&#10004;"></p>
+                        <label for="Title">Le titre du livre : </label><input class="champ" type="text" name="Title" placeholder="Ex : Harry Potter ..."/><br />
                         <label for ="Author">Auteur du livre : </label><input class="champ" type="text" name="Author" placeholder="Ex : Claude Ponti ..."/><br />
                         <label for="Editor">Editeur du livre : </label><input class="champ" type="text" name="Editor" placeholder="Ex : Hachette jeunesse ..."/><br /><br />
-                        <label>Langue : </label> 
+                        <label>Langue : </label>
                             <select name="Language" id="choix">
                                 <option value="default"></option>
                                 <option value="Français">Français</option>
                                 <option value="Anglais">Anglais</option>
-                            </select>
+                            </select><br><br>
                     </form>
                 </div>
-            </section>
+                ______________________________________
+                <div class="slideshow-container">
+                  <div class="mySlides fade">
+                    <img src="lib\styles\css\bibli1.jpg" style="width: 100%">
+                  </div>
 
-            <footer>
-            </footer>
+                  <div class="mySlides fade">
+                    <img src="lib\styles\css\bibli2.jpg" style="width: 100%">
+                  </div>
+
+                </div>
+                <br />
+
+                <div style="text-align:center">
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+              </div>
+
+              <script>
+                var slideIndex = 0;
+                showSlides();
+
+                function showSlides() {
+                  var i;
+                  var slides = document.getElementsByClassName("mySlides");
+                  var dots = document.getElementsByClassName("dot");
+                  for (i = 0; i < slides.length; i++) {
+                    slides[i].style.display = "none";
+                  }
+                  slideIndex++;
+                  if (slideIndex > slides.length) {slideIndex = 1}
+                  for (i = 0; i < dots.length; i++) {
+                    dots[i].className = dots[i].className.replace(" active", "");
+                  }
+                  slides[slideIndex-1].style.display = "block";
+                  dots[slideIndex-1].className += " active";
+                  setTimeout(showSlides, 5000); // Change image every 2 seconds
+                }
+            </script>
+    </section>
+    <footer>
+    </footer>
     </body>
 </html>
